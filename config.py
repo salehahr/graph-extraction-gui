e134f0514f5dd5d230576390360ffa108e8d618e
s <salehahr@users.noreply.github.com>
2022-09-02 13:19:51 +0200

    add functions for reading/updating dir in FileBrowser

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # data
 IMAGE_SIZE = 256
+IMAGE_DIRECTORY = "./img/"
 DEFAULT_FILEPATH = "./img/GRK011-0001_14480.png"
 
 # adjacency matrix predictor
--- gui/Sidebar.py
+++ gui/Sidebar.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import os
+from typing import TYPE_CHECKING, Optional
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
@@ -8,6 +9,8 @@ from PyQt5.QtWidgets import (
     QFileDialog,
     QGridLayout,
     QGroupBox,
+    QLabel,
+    QLineEdit,
     QPushButton,
     QVBoxLayout,
     QWidget,
@@ -15,6 +18,8 @@ from PyQt5.QtWidgets import (
 
 if TYPE_CHECKING:
     from .DataContainer import DataContainer
+
+from config import IMAGE_DIRECTORY
 
 
 class Sidebar(QDockWidget):
@@ -59,27 +64,55 @@ class SideBarWidget(QGroupBox):
 
 class FileBrowser(SideBarWidget):
     def __init__(self, *args, **kwargs):
+        self._directory_field: Optional[QLineEdit] = None
+        self._current_directory: str = IMAGE_DIRECTORY
+
         super().__init__(*args, **kwargs)
 
     def _init_layout(self):
         browse_button = QPushButton("Browse")
         browse_button.clicked.connect(self._browse)
 
+        self._directory_field = QLineEdit(self.current_directory)
+        self._directory_field.returnPressed.connect(self._update_directory)
+
         layout = QGridLayout()
-        layout.addWidget(browse_button, 0, 0)
+        layout.addWidget(QLabel("Directory:"), 0, 0)
+        layout.addWidget(self._directory_field, 1, 0)
+        layout.addWidget(browse_button, 2, 0)
 
         self.setLayout(layout)
 
     def _browse(self):
+        self._update_directory()
+
         dialog = QFileDialog()
         dialog.setFileMode(QFileDialog.ExistingFile)
-        dialog.setDirectory("./img/")
+        dialog.setDirectory(os.path.abspath(self.current_directory))
         dialog.setNameFilter("Images (*.png)")
         dialog.setViewMode(QFileDialog.Detail)
 
         if dialog.exec_():
             filenames = dialog.selectedFiles()
             self.current_filepath = filenames[0]
+
+    def _update_directory(self):
+        new_dir = self._directory_field.text()
+
+        if os.path.exists(new_dir):
+            self.current_directory = new_dir
+
+    @property
+    def current_directory(self):
+        return self._current_directory
+
+    @current_directory.setter
+    def current_directory(self, new_dir):
+        if os.path.abspath(self._current_directory) == os.path.abspath(new_dir):
+            return
+
+        self._current_directory = new_dir
+        self._directory_field.setText(self.current_directory)
 
     @property
     def current_filepath(self):
@@ -88,3 +121,6 @@ class FileBrowser(SideBarWidget):
     @current_filepath.setter
     def current_filepath(self, filepath):
         self._data_container.current_image_filepath = filepath
+        self.current_directory = os.path.split(
+            os.path.relpath(self.current_filepath, start="./")
+        )[0]
